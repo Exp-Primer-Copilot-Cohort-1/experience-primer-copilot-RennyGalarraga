b6a616fd8be7c53e1296f0ffdc1650c007598dcf
Renny Galárraga <122330979+RennyGalarraga@users.noreply.github.com>
2023-10-21 05:10:47 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,57 @@
+// Create web server using express.js
+//         "description": "VueJS is an open-source JavaScript framework for building user interfaces and single-page applications."
+//         }
+//     ];
+//     return skills;
+// }
+
+// function skillsMember() {
+//     var skills = [
+//         {
+//             "id": 1,
+//             "name": "HTML5",
+//             "level": 5,
+//             "description": "HTML5 is a markup language used for structuring and presenting content on the World Wide Web."
+//         },
+//         {
+//             "id": 2,
+//             "name": "CSS3",
+//             "level": 5,
+//             "description": "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language."
+//         },
+//         {
+//             "id": 3,
+//             "name": "Javascript",
+//             "level": 5,
+//             "description": "JavaScript is a high-level, dynamic, untyped, and interpreted programming language."
+//         },
+//         {
+//             "id": 4,
+//             "name": "jQuery",
+//             "level": 5,
+//             "description": "jQuery is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML."
+//         },
+//         {
+//             "id": 5,
+//             "name": "Bootstrap",
+//             "level": 5,
+//             "description": "Bootstrap is a free and open-source collection of tools for creating websites and web applications."
+//         },
+//         {
+//             "id": 6,
+//             "name": "PHP",
+//             "level": 4,
+//             "description": "PHP is a server-side scripting language designed for web development but also used as a general-purpose programming language."
+//         },
+//         {
+//             "id": 7,
+//             "name": "Laravel",
+//             "level": 4,
+//             "description": "Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications."
+//         },
+//         {
+//             "id": 8,
+//             "name": "MySQL",
+//             "level": 4,
+//             "description": "MySQL is an open-source relational database management system (RDBMS)."
+//
